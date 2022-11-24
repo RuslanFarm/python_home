3b740b8a602c43d36c470a80c4bfce23f61c0d69
Ruslan <ruslanfarm@yandex.ru>
2022-11-25 02:19:27 +0300

    Add file

--- ege/nomera 17/4414/4414.py
+++ ege/nomera 17/4414/4414.py
@@ -3,7 +3,7 @@ with open('4414.txt') as f:
     pairs = 0
     maxrazn = -999999999
     for i in range(len(nums)):
-        for j in range(i + 1, len(nums) - 1):
+        for j in range(i + 1, len(nums)):
             if ((nums[i] - nums[j]) % 36 == 0 and (nums[i] % 13 == 0 or nums[j] % 13 == 0)):
                 pairs += 1
                 maxrazn = max(maxrazn, nums[i] - nums[j])
