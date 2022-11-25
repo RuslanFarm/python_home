818e7dd79d4e8399d41796b7be74331ac9de8a4c
RuslanFarm <94564710+RuslanFarm@users.noreply.github.com>
2022-11-25 09:10:18 +0300

    Update 3693.py

--- ege/nomera 25/3693.py
+++ ege/nomera 25/3693.py
@@ -4,12 +4,20 @@ if int('1245') % 51 == 0:
     sp.append(int('1245'))
     ost.append(int(int('1245') / 51))
 
-for i in range(10):
-    if int('12' + str(i) + '45') % 51 == 0:
-        sp.append(int('1245' + str(i)))
-        ost.append(int(int('1245' + str(i)) / 51))
+if int('124500') % 51 == 0:
+    sp.append(int('124500'))
+    ost.append(int(int('124500') / 51))
 
-for i in range(10):
+if int('120045') % 51 == 0:
+    sp.append(int('120045'))
+    ost.append(int(int('120045')) / 51)
+
+for i in range(100):
+    if int('12' + str(i) + '45') % 51 == 0:
+        sp.append(int('12' + str(i) + '45'))
+        ost.append(int(int('12' + str(i) + '45') / 51))
+
+for i in range(100):
     if int('1245' + str(i)) % 51 == 0:
         sp.append(int('1245' + str(i)))
         ost.append(int(int('1245' + str(i)) / 51))
@@ -20,16 +28,4 @@ for i in range(10):
             sp.append(int('12' + str(i) + '45' + str(j)))
             ost.append(int(int('12' + str(i) + '45' + str(j)) / 51))
 
-
-for i in range(10):
-    for j in range(10):
-        if int('1245' + str(i) + str(j)) % 51 == 0:
-            sp.append(int('1245' + str(i) + str(j)))
-            ost.append(int(int('1245' + str(i) + str(j)) / 51))
-
-for i in range(10):
-    for j in range(10):
-        if int('12' + str(i) + str(j) + '45') % 51 == 0:
-            sp.append(int('1245' + str(i) + str(j)))
-            ost.append(int(int('1245' + str(i) + str(j)) / 51))
 print(sorted(sp), sorted(ost), sep='\n')
